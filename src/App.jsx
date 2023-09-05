d2b101c72c8f162ccd79046db603335b22372782
Nick-Rodrigues <RM551060@fiap.com.br>
2023-09-05 16:11:59 -0300

    Criação componente rodapé.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
   const [count, setCount] = useState(0)
