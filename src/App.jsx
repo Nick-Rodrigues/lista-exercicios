1bfca135ad22f87e51c53b2bf18f2c5cb09a1a82
Nick-Rodrigues <RM551060@fiap.com.br>
2023-09-05 16:08:51 -0300

    Menu para navegação criado e funcionando.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Menu from './components/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Menu/>
       <Outlet/>
     </>
   )
