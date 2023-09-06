07025ad7b620ba3d038a6a94618bd48ebad92b35
Nick-Rodrigues <rm551060@fiap.com.br>
2023-09-06 08:34:02 -0300

    Remoção tag setCount App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Menu from './components/Menu'
 import Rodape from './components/Rodape'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
